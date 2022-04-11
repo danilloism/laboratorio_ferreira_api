1550a089fc06ffdbb22aeb6cfc1e138b30c4f1c8
Danillo Ilggner <danilloilggner@gmail.com>
2022-04-11 17:49:58 -0300

    adicionar modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContatoModule } from './contato/contato.module';
+import { ServicoModule } from './servico/servico.module';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      entities: [`$__dirname/**/*.entity{.ts,.js}`],
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
       synchronize: true,
       database: 'lab_ferreira',
     }),
+    ContatoModule,
+    ServicoModule,
+    ProdutoModule,
   ],
   controllers: [],
   providers: [],
