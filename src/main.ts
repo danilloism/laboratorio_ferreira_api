b3399a184262aef3b2264cd54a5918c6e5c148f5
Danillo Ilggner <danilloilggner@gmail.com>
2022-04-27 18:03:58 -0300

    retirar winston

--- src/main.ts
+++ src/main.ts
@@ -4,38 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './modules/sistema/prisma';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
-import { utilities as WinstonUtilities, WinstonModule } from 'nest-winston';
-import * as Winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      level: 'silly',
-      handleRejections: true,
-      handleExceptions: true,
-      format: Winston.format.combine(
-        Winston.format.cli({ all: true, level: true }),
-        Winston.format.timestamp({ format: 'DD/MM/YYYY hh:mm:ss.SSS A' }),
-        Winston.format.ms(),
-        Winston.format.align(),
-        WinstonUtilities.format.nestLike('Laboratório Ferreira API', {
-          prettyPrint: true,
-        }),
-        Winston.format.colorize({
-          colors: {
-            error: 'red',
-            warn: 'yellow',
-            http: 'orange',
-            info: 'blue',
-            verbose: 'purple',
-            debug: 'green',
-            silly: 'pink',
-          },
-        }),
-      ),
-      transports: [new Winston.transports.Console()],
-    }),
-  });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
