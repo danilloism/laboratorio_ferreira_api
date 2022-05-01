63aeb0d70bb83f4cd2719da76e45c8f982b0be26
Danillo Ilggner <danilloilggner@gmail.com>
2022-05-01 00:25:48 -0300

    deletye garbage

--- src/modules/operacional/operacional.module.ts
+++ src/modules/operacional/operacional.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { EstoqueModule } from './estoque/estoque.module';
-import { ServicoModule } from './servico/servico.module';
-import { CaixaModule } from './caixa/caixa.module';
 
 @Module({
-  imports: [EstoqueModule, ServicoModule, CaixaModule],
+  imports: [],
 })
 export class OperacionalModule {}
--- src/modules/operacional/servico/servico.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ServicoService } from './servico.service';
-import { ServicoController } from './servico.controller';
-
-@Module({
-  controllers: [ServicoController],
-  providers: [ServicoService],
-})
-export class ServicoModule {}
