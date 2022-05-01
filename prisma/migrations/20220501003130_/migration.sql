cf9c6eaaa8c8af8ac5f13ed299256e96690d7d30
Danillo Ilggner <danilloilggner@gmail.com>
2022-05-01 14:56:21 -0300

    arrumando deploy do heroku

--- prisma/migrations/20220501003130_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `usa_esp_odont` on the `usuario` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "usuario" DROP COLUMN "usa_esp_odont";
