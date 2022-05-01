7d1937be49fa5ea37d1065f6596b17e68fb45318
Danillo Ilggner <danilloilggner@gmail.com>
2022-05-01 00:07:02 -0300

    mudança na estrategia de encriptação de senha

--- src/shared/helpers/password.helper.ts
+++ src/shared/helpers/password.helper.ts
@@ -1,6 +1,20 @@
 import * as bcrypt from 'bcrypt';
+import { v5 } from 'uuid';
 
 export class PasswordHelper {
-  static readonly encrypt = async (senha: string) =>
-    await bcrypt.hash(senha, parseInt(process.env.SALT_KEY));
+  constructor(private readonly senha: string) {}
+  private uuid = v5(process.env.UUID_NAME, process.env.UUID_NAMESPACE);
+
+  async encrypt() {
+    const encrypted = await bcrypt.hash(
+      this.senha + this.uuid,
+      parseInt(process.env.SALT_KEY),
+    );
+
+    return encrypted;
+  }
+
+  async compare(senhaEncriptada: string) {
+    return await bcrypt.compare(this.senha + this.uuid, senhaEncriptada);
+  }
 }
