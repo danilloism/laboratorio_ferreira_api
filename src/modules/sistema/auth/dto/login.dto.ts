40eeedd7bf99ef4255cca5613938f504e8cec26e
Danillo Ilggner <danilloilggner@gmail.com>
2022-05-03 11:44:28 -0300

    feat(auth): mudar dto pra classe

--- src/modules/sistema/auth/dto/login.dto.ts
+++ src/modules/sistema/auth/dto/login.dto.ts
@@ -1,7 +1,12 @@
-export interface LoginDto {
+import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class LoginDto {
+  @IsOptional()
   username?: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   senha: string;
 }
-
-//TODO: mudar para classe para validações
