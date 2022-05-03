663bd18f59056147dee778526a58a80d722cf867
Danillo Ilggner <danilloilggner@gmail.com>
2022-05-03 12:42:32 -0300

    feat(produto): criar resource

---
+++ src/modules/operacional/estoque/estoque.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { ProdutoModule } from './produto/produto.module';
+
+@Module({
+  imports: [ProdutoModule]
+})
+export class EstoqueModule {}
---
+++ src/modules/operacional/estoque/produto/enum/tipo-produto.enum.ts
@@ -0,0 +1,6 @@
+export enum TipoProduto {
+  PT = 'pt',
+  PPR = 'ppr',
+  PINO = 'pino',
+  RESTAURACAO = 'restauracao',
+}
