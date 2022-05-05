ac74a08a041545e8ac23bdbde3b35083255ccd53
Danillo Ilggner <danilloilggner@gmail.com>
2022-05-05 19:14:40 -0300

    deletar recursos de valor produto

--- src/modules/operacional/estoque/valor-produto/dto/update-valor-produto.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateValorProdutoDto } from './create-valor-produto.dto';
-
-export class UpdateValorProdutoDto extends PartialType(CreateValorProdutoDto) {}
