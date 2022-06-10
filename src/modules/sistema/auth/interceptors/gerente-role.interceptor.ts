2a62dd92993d4e2ff96b8499481228b1a7d0d399
Danillo Ilggner <danilloilggner@gmail.com>
2022-06-10 13:24:16 -0300

    feat: criar role interceptor de gerente

---
+++ src/modules/sistema/auth/interceptors/gerente-role.interceptor.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { CategoriaEnum } from '../../../agenda/contato/enums/categoria.enum';
+import { RoleInterceptor } from './role.interceptor';
+
+@Injectable()
+export class GerenteRoleInterceptor extends RoleInterceptor {
+  constructor() {
+    super(CategoriaEnum.GERENTE);
+  }
+}
