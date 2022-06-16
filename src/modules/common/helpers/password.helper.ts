72dcf1ecf0303a0159ab52c6765ad60045df751e
Danillo Ilggner <danilloilggner@gmail.com>
2022-06-15 21:22:31 -0300

    feat: adicionar v2 de password helper

--- src/modules/common/helpers/password.helper.ts
+++ src/modules/common/helpers/password.helper.ts
@@ -18,3 +18,19 @@ export class PasswordHelper {
     return await bcrypt.compare(this.senha + this.uuid, senhaEncriptada);
   }
 }
+
+export class PasswordHelperV2 {
+  private readonly uuid = v5(process.env.UUID_NAME, process.env.UUID_NAMESPACE);
+
+  static async encrypt(senha: string) {
+    const encrypted = await bcrypt.hash(
+      senha + new PasswordHelperV2().uuid,
+      parseInt(process.env.SALT_KEY),
+    );
+
+    return encrypted;
+  }
+
+  static readonly compare = async (senha: string, senhaEncriptada: string) =>
+    await bcrypt.compare(senha + new PasswordHelperV2().uuid, senhaEncriptada);
+}
