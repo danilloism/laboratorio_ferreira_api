2b892246ce2167ea221c3f09afb94455a989bdcd
Danillo Ilggner <danilloilggner@gmail.com>
2022-06-24 07:48:52 -0300

    feat: db.config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Logger, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AgendaModule } from './modules/agenda/agenda.module';
 import { EstoqueModule } from './modules/estoque/estoque.module';
@@ -11,26 +11,19 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { FinanceiroModule } from './modules/financeiro/financeiro.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
+import { dbConfig } from './config/db.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
     TerminusModule,
     HttpModule,
-    ConfigModule.forRoot({ isGlobal: true, cache: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadEntities: true,
-      logNotifications: true,
-      logger: 'debug',
-      synchronize: true,
-      applicationName: 'lab-ferreira-api',
-      namingStrategy: new SnakeNamingStrategy(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (): Promise<TypeOrmModuleOptions> => dbConfig(),
     }),
+    // TypeOrmModule.forRoot(dbConfig),
     AuthModule,
     AgendaModule,
     EstoqueModule,
