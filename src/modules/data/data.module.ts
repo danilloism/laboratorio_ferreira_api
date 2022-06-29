eb5a9c9f15eb9613698747c445cb078ba39cbb52
Danillo Ilggner <danilloilggner@gmail.com>
2022-06-29 14:14:04 -0300

    feat(prisma): adicionar prisma e migrations

---
+++ src/modules/data/data.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaHealthIndicator } from 'src/modules/data/services/prisma.health-indicator';
+import { PrismaService } from 'src/modules/data/services/prisma.service';
+
+@Module({
+  providers: [PrismaService, PrismaHealthIndicator],
+  exports: [PrismaHealthIndicator, PrismaService],
+})
+export class DataModule {}
