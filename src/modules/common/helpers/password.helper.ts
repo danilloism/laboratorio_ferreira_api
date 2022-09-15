a395fb99e5a37b6421db075ddf6be9548b247633
Danillo Ilggner <danilloilggner@gmail.com>
2022-09-15 12:13:12 -0300

    refactor: pawwordhelper

--- src/modules/common/helpers/password.helper.ts
+++ src/modules/common/helpers/password.helper.ts
@@ -1,26 +1,26 @@
 import * as bcrypt from 'bcrypt';
 import { v5 } from 'uuid';
 
+// export class PasswordHelper {
+//   constructor(private readonly senha: string) {}
+//
+//   private uuid = v5(process.env.UUID_NAME, process.env.UUID_NAMESPACE);
+//
+//   async encrypt() {
+//     const encrypted = await bcrypt.hash(
+//       this.senha + this.uuid,
+//       parseInt(process.env.SALT_KEY),
+//     );
+//
+//     return encrypted;
+//   }
+//
+//   async compare(senhaEncriptada: string) {
+//     return await bcrypt.compare(this.senha + this.uuid, senhaEncriptada);
+//   }
+// }
+
 export class PasswordHelper {
-  constructor(private readonly senha: string) {}
-
-  private uuid = v5(process.env.UUID_NAME, process.env.UUID_NAMESPACE);
-
-  async encrypt() {
-    const encrypted = await bcrypt.hash(
-      this.senha + this.uuid,
-      parseInt(process.env.SALT_KEY),
-    );
-
-    return encrypted;
-  }
-
-  async compare(senhaEncriptada: string) {
-    return await bcrypt.compare(this.senha + this.uuid, senhaEncriptada);
-  }
-}
-
-export class PasswordHelperV2 {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {}
 
